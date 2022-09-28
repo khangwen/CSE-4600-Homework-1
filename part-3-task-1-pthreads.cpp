723a27ad14f2b24e47250c5d0f68de1396c87478
Alan Nguyen <a_wen197@yahoo.com>
2022-09-27 19:25:06 -0700

    added 3rd thread with new functions on all threads

--- part-3-task-1-pthreads.cpp
+++ part-3-task-1-pthreads.cpp
@@ -7,35 +7,78 @@
 
 #include <pthread.h>
 #include <stdio.h>
+#include <iostream>
+#include <time.h>
 
 using namespace std;
 
-//The thread
-void * thread_func (void *data)
+// The thread
+void *thread_func_1(void *data)
 {
-  char *tname = (char *) data;
+    char *tname = (char *)data;
 
-  printf("My thread identifier is %s\n", tname);
+    // print thread name
+    while (1)
+        printf("My thread identifier is %s\n", tname);
 
-  pthread_exit (0);
+    pthread_exit(0);
 }
 
-int main ()
+void *thread_func_2(void *arg)
 {
-  pthread_t id1, id2;           //thread identifiers
-  pthread_attr_t attr1, attr2;  //set of thread attributes
-  char *tnames[2] = { "Thread 1", "Thread 2" }; //names of threads
-  //get the default attributes
-  pthread_attr_init (&attr1);
-  pthread_attr_init (&attr2);
+    int counter = 1;
 
-  //create the threads
-  pthread_create (&id1, &attr1, thread_func, tnames[0]);
-  pthread_create (&id2, &attr2, thread_func, tnames[1]);
+    // Increment counter every cycle
+    while (1)
+    {
+        counter += 1;
+        cout << "Curent Counter: " << counter << "\n";
+    }
 
-  //wait for the threads to exit
-  pthread_join (id1, NULL);
-  pthread_join (id2, NULL);
+    pthread_exit(0);
+}
 
-  return 0;
+void *thread_func_3(void *arg)
+{
+    int number;
+
+    // initialize seed
+    srand(time(NULL));
+
+    while (1)
+    {
+        // generate random number from 1 to 2
+        number = rand() % 2 + 1;
+
+        // if 1, print heads, if 2 print tails
+        if (number == 1)
+            cout << "Heads\n";
+        else if (number == 2)
+            cout << "Tails\n";
+    }
+
+    pthread_exit(0);
+}
+
+int main()
+{
+    pthread_t id1, id2, id3;                                // thread identifiers
+    pthread_attr_t attr1, attr2, attr3;                     // set of thread attributes
+    char *tnames[3] = {"Thread 1", "Thread 2", "Thread 3"}; // names of threads
+    // get the default attributes
+    pthread_attr_init(&attr1);
+    pthread_attr_init(&attr2);
+    pthread_attr_init(&attr3);
+
+    // create the threads
+    pthread_create(&id1, &attr1, thread_func_1, tnames[0]);
+    pthread_create(&id2, &attr2, thread_func_2, NULL);
+    pthread_create(&id3, &attr3, thread_func_3, NULL);
+
+    // wait for the threads to exit
+    pthread_join(id1, NULL);
+    pthread_join(id2, NULL);
+    pthread_join(id3, NULL);
+
+    return 0;
 }
